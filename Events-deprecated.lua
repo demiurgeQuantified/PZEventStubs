912525456946683d79a268af7d6fc5d3a551c8cf
demiurgeQuantified <>
2023-05-26 17:08:01 +0000

    Updated by GitHub action

---
+++ Events-deprecated.lua
@@ -0,0 +1,274 @@
+Events = {}
+---@deprecated
+---(Deprecated) 
+Events.OnAIStateEnter = {
+    ---@param func function
+    Add = function(func) end,
+    ---@param func function
+    Remove = function(func) end,
+}
+---@deprecated
+---(Deprecated) 
+Events.OnAIStateExecute = {
+    ---@param func function
+    Add = function(func) end,
+    ---@param func function
+    Remove = function(func) end,
+}
+---@deprecated
+---(Deprecated) 
+Events.OnAIStateExit = {
+    ---@param func function
+    Add = function(func) end,
+    ---@param func function
+    Remove = function(func) end,
+}
+---@deprecated
+---(Deprecated) 
+Events.OnAddBuilding = {
+    ---@param func function
+    Add = function(func) end,
+    ---@param func function
+    Remove = function(func) end,
+}
+---@deprecated
+---(Deprecated) 
+Events.OnBeingHitByZombie = {
+    ---@param func function
+    Add = function(func) end,
+    ---@param func function
+    Remove = function(func) end,
+}
+---@deprecated
+---(Deprecated) 
+Events.OnChangeWeather = {
+    ---@param func function
+    Add = function(func) end,
+    ---@param func function
+    Remove = function(func) end,
+}
+---@deprecated
+---(Deprecated) 
+Events.OnCharacterCreateStats = {
+    ---@param func function
+    Add = function(func) end,
+    ---@param func function
+    Remove = function(func) end,
+}
+---@deprecated
+---(Deprecated) 
+Events.OnCharacterMeet = {
+    ---@param func function
+    Add = function(func) end,
+    ---@param func function
+    Remove = function(func) end,
+}
+---@deprecated
+---(Deprecated) 
+Events.OnDawn = {
+    ---@param func function
+    Add = function(func) end,
+    ---@param func function
+    Remove = function(func) end,
+}
+---@deprecated
+---(Deprecated) 
+Events.OnDoTileBuilding = {
+    ---@param func function
+    Add = function(func) end,
+    ---@param func function
+    Remove = function(func) end,
+}
+---@deprecated
+---(Deprecated) 
+Events.OnDusk = {
+    ---@param func function
+    Add = function(func) end,
+    ---@param func function
+    Remove = function(func) end,
+}
+---@deprecated
+---(Deprecated) 
+Events.OnIsoThumpableLoad = {
+    ---@param func function
+    Add = function(func) end,
+    ---@param func function
+    Remove = function(func) end,
+}
+---@deprecated
+---(Deprecated) 
+Events.OnIsoThumpableSave = {
+    ---@param func function
+    Add = function(func) end,
+    ---@param func function
+    Remove = function(func) end,
+}
+---@deprecated
+---(Deprecated) 
+Events.OnLoginState = {
+    ---@param func function
+    Add = function(func) end,
+    ---@param func function
+    Remove = function(func) end,
+}
+---@deprecated
+---(Deprecated) 
+Events.OnLoginStateSuccess = {
+    ---@param func function
+    Add = function(func) end,
+    ---@param func function
+    Remove = function(func) end,
+}
+---@deprecated
+---(Deprecated) 
+Events.OnMakeItem = {
+    ---@param func function
+    Add = function(func) end,
+    ---@param func function
+    Remove = function(func) end,
+}
+---@deprecated
+---(Deprecated) 
+Events.OnMapLoadCreateIsoObject = {
+    ---@param func function
+    Add = function(func) end,
+    ---@param func function
+    Remove = function(func) end,
+}
+---@deprecated
+---(Deprecated) 
+Events.OnNewSurvivorGroup = {
+    ---@param func function
+    Add = function(func) end,
+    ---@param func function
+    Remove = function(func) end,
+}
+---@deprecated
+---(Deprecated) 
+Events.OnNPCSurvivorUpdate = {
+    ---@param func function
+    Add = function(func) end,
+    ---@param func function
+    Remove = function(func) end,
+}
+---@deprecated
+---(Deprecated) 
+Events.OnPlayerSetSafehouse = {
+    ---@param func function
+    Add = function(func) end,
+    ---@param func function
+    Remove = function(func) end,
+}
+---@deprecated
+---(Deprecated) 
+Events.OnPostCharactersSquareDraw = {
+    ---@param func function
+    Add = function(func) end,
+    ---@param func function
+    Remove = function(func) end,
+}
+---@deprecated
+---(Deprecated) 
+Events.OnPostFloorSquareDraw = {
+    ---@param func function
+    Add = function(func) end,
+    ---@param func function
+    Remove = function(func) end,
+}
+---@deprecated
+---(Deprecated) 
+Events.OnPostTileDraw = {
+    ---@param func function
+    Add = function(func) end,
+    ---@param func function
+    Remove = function(func) end,
+}
+---@deprecated
+---(Deprecated) 
+Events.OnPostTilesSquareDraw = {
+    ---@param func function
+    Add = function(func) end,
+    ---@param func function
+    Remove = function(func) end,
+}
+---@deprecated
+---(Deprecated) 
+Events.OnPostWallSquareDraw = {
+    ---@param func function
+    Add = function(func) end,
+    ---@param func function
+    Remove = function(func) end,
+}
+---@deprecated
+---(Deprecated) 
+Events.OnPreGameStart = {
+    ---@param func function
+    Add = function(func) end,
+    ---@param func function
+    Remove = function(func) end,
+}
+---@deprecated
+---(Deprecated) 
+Events.OnRadioInteraction = {
+    ---@param func function
+    Add = function(func) end,
+    ---@param func function
+    Remove = function(func) end,
+}
+---@deprecated
+---(Deprecated) 
+Events.OnRainStart = {
+    ---@param func function
+    Add = function(func) end,
+    ---@param func function
+    Remove = function(func) end,
+}
+---@deprecated
+---(Deprecated) 
+Events.OnRainStop = {
+    ---@param func function
+    Add = function(func) end,
+    ---@param func function
+    Remove = function(func) end,
+}
+---@deprecated
+---(Deprecated) 
+Events.OnRenderUpdate = {
+    ---@param func function
+    Add = function(func) end,
+    ---@param func function
+    Remove = function(func) end,
+}
+---@deprecated
+---(Deprecated) 
+Events.OnVehicleHorn = {
+    ---@param func function
+    Add = function(func) end,
+    ---@param func function
+    Remove = function(func) end,
+}
+---@deprecated
+---(Deprecated) 
+Events.OnWorldMessage = {
+    ---@param func function
+    Add = function(func) end,
+    ---@param func function
+    Remove = function(func) end,
+}
+Hook = {}
+---@deprecated
+---(Deprecated) 
+Hook.UseItem = {
+    ---@param func function
+    Add = function(func) end,
+    ---@param func function
+    Remove = function(func) end,
+}
+---@deprecated
+---(Deprecated) 
+Hook.WeaponSwingHitPoint = {
+    ---@param func function
+    Add = function(func) end,
+    ---@param func function
+    Remove = function(func) end,
+}
